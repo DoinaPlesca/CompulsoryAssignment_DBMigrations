e02c029809ed6f440b553fd5fa5535b967b8c563
Doina Plesca <doinitzaplesca@yahoo.com>
2025-02-25 14:41:47 +0100

    change course credits data type from int to decimal,generate migration + artifact;update db

---
+++ Migrations/V7__ModifyCourseCredits.sql
@@ -0,0 +1,8 @@
+﻿START TRANSACTION;
+ALTER TABLE "Courses" ALTER COLUMN "Credits" TYPE numeric;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20250225131706_ModifyCourseCredits', '9.0.2');
+
+COMMIT;
+
